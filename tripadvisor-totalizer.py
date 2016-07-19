e57d59bdb325eeb3002c87e872c3de06f1bcd98e
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2016-07-19 14:30:51 +0200

    [feature] implement directory creation

--- tripadvisor-totalizer.py
+++ tripadvisor-totalizer.py
@@ -1,6 +1,45 @@
 import argparse
 import logging
+import time
+import os
 
+def get_subdirectories(rootdir):
+    return [x[0] for x in os.walk(rootdir)]
+
+# Creates a directory for a session
+def create_session_directory(directory_name):
+    # Build directory name
+    directory_path = os.getcwd() + '/totalized/' + directory_name
+
+    logger.info('STARTED: Creation of directory ' + os.getcwd() + '/totalized/' + directory_name)
+
+    # Create the folder
+    os.makedirs(directory_path)
+
+    logger.info('FINISHED: Creation of directory ' + os.getcwd() + '/totalized/' + directory_name)
+
+    return directory_path
+
+# Creates a directory for each rating category (e.g. 5 stars, 4 stars)
+def create_rating_directories(path):
+    stars = [1, 2, 3, 4, 5]
+
+    paths = list()
+
+    for star in stars:
+        # Build directory name
+        directory_path = path + '/' + str(star) + '-star'
+
+        logger.info('STARTED: Creation of directory ' + directory_path)
+
+        # Create the folder
+        os.makedirs(directory_path)
+
+        logger.info('FINISHED: Creation of directory ' + directory_path)
+
+        paths.append(directory_path)
+
+    return paths
 
 # Main
 if __name__ == '__main__':
@@ -14,7 +53,13 @@ if __name__ == '__main__':
     logger = logging.getLogger(__name__)
     logging.getLogger().addHandler(logging.StreamHandler())
 
+    source_path = args.path
+    source_folder = os.path.basename(os.path.normpath(source_path))
 
+    target_directory = create_session_directory(source_folder)
+    star_directories = create_rating_directories(target_directory)
+
+    sub_directory_paths = get_subdirectories(source_path)
 
 
 
